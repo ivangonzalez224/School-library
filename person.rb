58b1d44b0231610801556eb32bc85547b4864b6a
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-10-03 13:50:08 -0500

    Check decorators in action

--- person.rb
+++ person.rb
@@ -1,4 +1,7 @@
 require './nameable'
+require './decorator'
+require './capitalize_decorator'
+require './trimmer_decorator'
 
 class Person
   attr_reader :id
@@ -15,13 +18,19 @@ class Person
     of_age? || @parent_permission
   end
 
+  def correct_name
+    @name
+  end
+
   private
 
   def of_age?
     @age >= 18
   end
-
-  def correct_name
-    @name
-  end
 end
+
+person = Person.new(22, 'maximilianus')
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
