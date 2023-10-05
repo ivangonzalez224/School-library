5059a6d745b1611c9d8bc2c5873d963351c54524
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-10-05 11:10:45 -0500

    Add the methods to create a teacher or a student and to list all people

--- app.rb
+++ app.rb
@@ -24,4 +24,41 @@ class App
   def list_all_books
     @books.each { |book| puts "Title: #{book.title}, Author: #{book.author}" }
   end
+
+  def create_person
+    puts 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
+    person_class = gets.chomp
+    result_arr = getting_name_age
+    if person_class == '1'
+      print 'Classroom: '
+      student_classroom = gets.chomp
+      print 'Has parent permission? [Y/N]: '
+      parent_permission = gets.chomp.downcase
+      parent_permission = parent_permission == 'y'
+      new_student = Student.new(result_arr[0], result_arr[1], student_classroom, parent_permission)
+      @people << new_student
+    else
+      print 'Specialization: '
+      teacher_specialization = gets.chomp
+      new_teacher = Teacher.new(result_arr[0], result_arr[1], teacher_specialization)
+      @people << new_teacher
+    end
+    puts 'Person created succesfully'
+  end
+
+  def getting_name_age
+    common_arr = []
+    print 'Age: '
+    person_age = gets.chomp.to_i
+    print 'Name: '
+    person_name = gets.chomp
+    common_arr << person_age
+    common_arr << person_name
+  end
+
+  def list_all_people
+    @people.each do |person_item|
+      puts "[#{person_item.class}] Name: #{person_item.name}, ID: #{person_item.id}, Age: #{person_item.age}"
+    end
+  end
 end
