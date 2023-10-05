afce90d5ba7ef6022475d6dafbc5a61c9247ba13
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-10-05 08:33:18 -0500

    Add the methods to create a book and to list all books

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 require './book'
-require '.person/'
-requie './rental'
+require './person'
+require './rental'
 
 class App
   attr_accessor :books, :people, :rentals
@@ -9,5 +9,19 @@ class App
     @books = []
     @people = []
     @rentals = []
+  end
+
+  def create_book
+    print 'Title: '
+    book_title = gets.chomp
+    print 'Author: '
+    book_author = gets.chomp
+    new_book = Book.new(book_title, book_author)
+    @books << new_book
+    puts 'Book created successfully'
+  end
+
+  def list_all_books
+    @books.each { |book| puts "Title: #{book.title}, Author: #{book.author}" }
   end
 end
