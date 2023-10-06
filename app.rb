c2cbc872d9f29d23e78fc6bb6c394fc203b30f3b
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-10-06 10:21:49 -0500

    Introduce a bug

--- app.rb
+++ app.rb
@@ -37,7 +37,7 @@ class App
       print 'Has parent permission? [Y/N]: '
       parent_permission = gets.chomp.downcase
       parent_permission = parent_permission == 'y'
-      new_student = Student.new(result_arr[0], result_arr[1], student_classroom, parent_permission: parent_permission)
+      new_student = Student.new(result_arr[0], result_arr[1], student_classroom, parent_permission)
       @people << new_student
     else
       print 'Specialization: '
